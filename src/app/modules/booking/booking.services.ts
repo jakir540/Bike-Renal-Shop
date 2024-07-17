3b69d46b93df0b015d95297796934666f8eee31c
jakir540 <jakirhossainmd540@gamil.com>
2024-07-17 18:30:07 +0600

    booking services functionality added

--- src/app/modules/booking/booking.services.ts
+++ src/app/modules/booking/booking.services.ts
@@ -73,7 +73,97 @@ const createBookingIntoDB = async (req: Request) => {
 };
 
 // return booking rental
-const returnRentalIntoDB = async() > {};
+const returnRentalIntoDB = async (id: string) => {
+  // check the bike exists or not
+  const isBookingBikeExits = await Booking.findById(id);
+
+  if (!isBookingBikeExits) {
+    throw new AppError(httpStatus.NOT_FOUND, 'rental bike not found');
+  }
+
+  // get the bike
+  const bike = await Bike.findById(isBookingBikeExits.bikeId);
+  if (!bike) {
+    throw new AppError(httpStatus.NOT_FOUND, 'bike not found');
+  }
+
+  // get the start time from the booking
+  const startTime = new Date(isBookingBikeExits.startTime);
+  // return the current time
+  const returnTime = new Date();
+  if (startTime >= returnTime) {
+    throw new AppError(
+      httpStatus.FORBIDDEN,
+      'start time must be before return time',
+    );
+  }
+
+  const timeDifferent =
+    Math.ceil(returnTime.getTime() - startTime.getTime()) / (1000 * 60 * 60);
+
+  // taking the price per hour of the bike
+  const pricePerHour = bike.pricePerHour;
+  //calculate the totla cost
+  const totalCost = timeDifferent * pricePerHour;
+
+  // for use session for more than one write operation
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+
+    const updateBooking = await Booking.findByIdAndUpdate(
+      id,
+      {
+        returnTime: returnTime,
+        totalCost: totalCost,
+        isReturned: true,
+      },
+      {
+        new: true,
+      },
+    ).session(session);
+
+    // if errro happens when update booking operation
+
+    if (!updateBooking) {
+      throw new AppError(
+        httpStatus.NOT_IMPLEMENTED,
+        'Failed to updating booking rental ',
+      );
+    }
+
+    // update bike isAvailable to true operatoin 02
+
+    const updateBikeAvailability = await Bike.findByIdAndUpdate(
+      isBookingBikeExits.bikeId,
+      { isAvailable: true },
+    ).session(session);
+
+    if (!updateBikeAvailability) {
+      throw new AppError(
+        httpStatus.NOT_IMPLEMENTED,
+        'Failed to update bike availability',
+      );
+    }
+
+    //commit transaction
+
+    await session.commitTransaction();
+    // end transaction
+    await session.endSession();
+    return updateBooking;
+  } catch (error) {
+    console.log(error);
+    await session.abortTransaction();
+    await session.endSession();
+
+    throw new AppError(
+      httpStatus.NOT_IMPLEMENTED,
+      'Failed to updationg return booking',
+    );
+  }
+};
 
 // return booking rental
 const getAllRentalOfUserIntoDB = async (req: Request) => {
